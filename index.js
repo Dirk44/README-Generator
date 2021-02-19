c2636d398f68ae108fd0bf32b00fc06767d1ee94
Derrick deVilliers <derrickd44@gmail.com>
2021-02-19 15:43:32 -0500

    Added variables

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const li = require('./li');
+const year = new Date().getFullYear()
+const ui = new inquirer.ui.BottomBar();
+
+ui.log.write('add line breaks with <br/>')
 
 
 inquirer.prompt([
@@ -9,79 +13,130 @@ inquirer.prompt([
         name: 'userName',
         message: 'What is your User name?',
         // Make sure User provided value
-        validate: (value) => {if(value){return true} else {return "need input to continue"}}
+        validate: (value) => { if (value) { return true } else { return "need input to continue" } }
     },
     {
         type: 'input',
         name: 'projectTitle',
         message: 'What is the Title of your Project?',
-        validate: (value) => {if(value){return true} else {return "need input to continue"}}
+        validate: (value) => { if (value) { return true } else { return "need input to continue" } }
     },
     {
         type: 'input',
         name: 'description',
         message: 'Give a Project Description.',
-        validate: (value) => {if(value){return true} else {return "need input to continue"}}
+        validate: (value) => { if (value) { return true } else { return "need input to continue" } }
     },
     {
         type: 'input',
         name: 'instaInstr',
         message: 'Installation instructions?',
-        validate: (value) => {if(value){return true} else {return "need input to continue"}}
+        validate: (value) => { if (value) { return true } else { return "need input to continue" } }
     },
     {
         type: 'input',
         name: 'usage',
-        message: 'Usage information?',
-        validate: (value) => {if(value){return true} else {return "need input to continue"}}
+        message: 'Usage information?'
+
     },
     {
         type: 'input',
         name: 'contrib',
-        message: 'Contribution guidelines?',
-        validate: (value) => {if(value){return true} else {return "need input to continue"}}
+        message: 'Contribution guidelines?'
+
     },
     {
         type: 'input',
         name: 'test',
-        message: 'Test instructions?',
-        validate: (value) => {if(value){return true} else {return "need input to continue"}}
+        message: 'Test instructions?'
     },
     {
         type: 'list',
         message: 'License?',
-        name: 'license',
-        choices: ['MIT', 'GPLv3', 'CC-0', 'BSD-3-Clause', 'WTFPL'],
-        validate: (value) => {if(value){return true} else {return "need input to continue"}}
+        name: 'licenseBadge',
+        choices: ['MIT', 'GPLv3', 'CC-0', 'BSD-3-Clause', 'WTFPL']
+
+    },
+    {
+        type: 'list',
+        message: 'License?',
+        name: 'licenseText',
+        choices: ['MIT', 'GPLv3', 'CC-0', 'BSD-3-Clause', 'WTFPL']
+
     },
     {
         type: 'input',
         name: 'gitHub',
-        message: 'What is your GitHub username?',
-        validate: (value) => {if(value){return true} else {return "need input to continue"}}
+        message: 'What is your GitHub username?'
+
     },
     {
         type: 'input',
         name: 'email',
-        message: 'What is your Email address?',
-        validate: (value) => {if(value){return true} else {return "need input to continue"}}
+        message: 'What is your Email address?'
+
     },
 ])
 
-.then(({
-   userName,
-   projectTitle,
-   description,
-   instaInstr,
-   usage,
-   contrib,
-   test,
-   license,
-   gitHub,
-   email
-}) =>{
-    //Template
-    const template =
-    
-    
-})
+    .then(() => {
+
+        const fileName = `./READMEs/ ${data.projectTitle}.md`;
+        const userName = data.userName;
+        const projectTitle = data.projectTitle;
+        const description = data.description;
+        const instaInstr = data.instaInstr;
+        const usage = data.usage;
+        const contrib = data.contrib;
+        const test = data.test;
+        const licenseBadge = li.choseBadge(license);
+        const licenseText = li.choseLicense(license);
+        const gitHub = data.gitHub;
+        const email = data.email;
+
+
+
+        //Template
+        const template = `# ${title}
+            
+## Description
+        ${ description}
+## Table of Contents
+        [Installation](#Installation)
+        [Usage](#Usage)
+        [License](#License)
+        [Contributing](#Contributing)
+        [Tests](#Tests)
+        [Questions](#Questions)
+## Installation
+        ${ instaInstr}
+## Usage
+        ${ usage}
+## License
+        This work is covered under ${ licenseText}
+## Contributing
+        ${ contrib}
+## Tests
+        ${ test};
+
+        ## Questions
+Please reach-out to me on [GitHub](http://www.github.com/${gitHub}) or email me at: [${email}](mailto:${email})
+---
+© ${year} ${userName}`;
+
+
+
+        //function to create readme
+        createNewFile(title, template);
+        function createNewFile(fileName, data) {
+
+            fs.writeFile(`./${fileName.toLowerCase().split(" ").join(" ")}.md`, data, () => {
+                if (err) {
+                    console.log(err);
+                }
+                console.log("Youre README has been generated");
+            })
+
+        }
+    });
+
+
---
+++ li.js
@@ -0,0 +1,36 @@
+function choseBadge(license) {
+    switch (license) {
+        case 'MIT':
+            return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`;
+        case 'GPLv3':
+            return `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`;
+        case 'CC-0':
+            return `[![License: CC0-1.0](https://licensebuttons.net/l/zero/1.0/80x15.png)](http://creativecommons.org/publicdomain/zero/1.0/)`;
+        case 'BSD-3-Clause':
+            return `[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)`;
+        case 'WTFPL':
+            return `[![License: WTFPL](https://img.shields.io/badge/License-WTFPL-brightgreen.svg)](http://www.wtfpl.net/about/)`;
+        default:
+    }
+};
+
+function choseLicense(license) {
+    switch (license) {
+        case 'MIT':
+            return `[The MIT License](https://opensource.org/licenses/MIT)`;
+        case 'GPLv3':
+            return `the [GNU GPL v3](https://www.gnu.org/licenses/gpl-3.0) license`;
+        case 'CC-0':
+            return `the [Creative Commons Zero](http://creativecommons.org/publicdomain/zero/1.0/) license`;
+        case 'BSD-3-Clause':
+            return `the [BSD 3-Clause License](https://opensource.org/licenses/BSD-3-Clause)`;
+        case 'WTFPL':
+            return `the [The Do What the Fuck You Want to Public License HAHAHA!](http://www.wtfpl.net/about/)`;
+        default:
+    }
+};
+
+module.exports = {
+    choseBadge,
+    choseLicense
+};
